c2702a8f39d0ef6d8f510306638fd053550f28cd
Brendon Body <bbody.project@gmail.com>
2013-12-30 21:55:11 +1100

    Anonymised, added heaps of features.

--- js/details.js
+++ js/details.js
@@ -1,13 +1,66 @@
-// Put your details here
-var name = "Brendon Body";
-var loc = "Sydney, Australia";
+// Details
+var name = "John Doe";
+var loc = "Timbuktu";
+var lookingfor = "Raking in the money";
+var pdfLink = "http://en.wikipedia.org/wiki/R%C3%A9sum%C3%A9";
 
-var employment = "Interning/Looking for opportunities";
+// Settings
+var showForks = true;
 
-var githubUsername = "bbody";
-var bitbucketUsername = "bbody";
-var pdfLink = "https://www.dropbox.com/s/j6e4jv7tkd5097h/CV.pdf";
+// Github and Bitbucket accounts
+var githubUsername = "example";
+var bitbucketUsername = "example";
+var githubURL = "https://github.com/" + githubUsername;
+var bitbucketURL = "https://bitbucket.org/" + bitbucketUsername;
 
+// If left blank e.g. url = "" then it won't appear
+var websiteURL = "http://www.example.com/";
+var blogURL = "";
+var portfolioURL = "";
+var twitterURL = "https://twitter.com/";
+var facebookURL = "";
+var skypeAccount = "test";
+var socialMedia = [["Github", githubURL], ["Bitbucket", bitbucketURL], ["Website", websiteURL], 
+	["Blog", blogURL], ["Portfolio", portfolioURL], ["Twitter", twitterURL], ["Facebook", facebookURL],
+	["Skype", skypeAccount]];
 
+// Education list
+var education = [["University of Queensland", "Bachelor of Engineering (Software Engineering)", "Graduate July, 2014"],
+	["Summer University", "Summer Program", "June-July 2013"],["Tianjin University", "Research", "June-July 2012"],
+	["Chinese University of Hong Kong", "Exchange Student", "September-December 2011"],
+	["Beijing University of Internation Relations", "Language course", "July-August"]];
 
-var education = [["University of Queensland", "Bachelor of Engineering (Software Engineering)", "Graduate July, 2014"],["Summer University", "Summer Program", "June-July 2013"],["Tianjin University", "Research", "June-July 2012"],["Chinese University of Hong Kong", "Exchange Student", "September-December 2011"],["Beijing University of Internation Relations", "Language course", "July-August"]];
+// Employment list
+var employment = [["Westpac Banking Group", "Software Engineering Intern", "November 2013 - February 2014"],
+	["APS Mining", "Test Engineering Intern", "December 2012 - November 2013"],
+	["Keays Software", "Software Engineering Intern", "April 2012 - November 2013"],
+	["Google Australia", "Student Ambassador", "January 2013 - November 2013"],
+	["Wali Information Technology", "IT Intern", "January 2012 - February 2012"]];
+
+// Volunteering list
+var volunteering = [["Mentor", "Coder Dojo Brisbane", "July - September 2013"],
+	["Director of IT", "Australia China Youth Association (National)", "January - September 2013"],
+	["Rescue Referee", "RoboCup Junior Australia", "August 2012"]];	
+
+// Awards list
+var awards = [["HSK Level 5", "Hanban Institute", "December 2013"],
+	["Certificate of Achievement", "ACM South Pacific Regional Programming Contest", "2013"],
+	["Dean's Commendation", "The University of Queensland", "2011 and 2012"]];
+
+// Membership list
+var membership = [["Australian Computer Society", "Associate Member"],
+	["Engineers Australia", "Student Member"],
+	["Institute of Electrical and Electronics Engineers", "Student Member"]];
+
+// Skills list
+var skillsLanguages = [["Proficient", "C/C++ Java Python"],
+	["Experienced", "C# .NET"],
+	["Familiar", "PHP Javascript Bash"]];
+
+var skillsTools = [["Proficient", "Git Wordpress"],
+	["Experienced", "Android Github"],
+	["Familiar", "TCP/IP SVN"]];
+
+var skillsConcepts = [["Proficient", "Kanban Agile"],
+	["Experienced", "Functional Testing"],
+	["Familiar", "Functional Programming"]];
--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,13 @@
+String.prototype.replaceAll = function(search, replace)
+{
+    //if replace is null, return original string otherwise it will
+    //replace search string with 'undefined'.
+    if(!replace) 
+        return this;
+
+    return this.replace(new RegExp('[' + search + ']', 'g'), replace);
+};
+
 
 function github(username){
     var mess = "Repositories:\n";
@@ -27,7 +37,117 @@ function github(username){
  return mess;
 }
 
+// Variables
+var title = "Welcome to " + name + "'s résumé. Enter command `help` for command list";
+
+// Command map
+var commandMap = {};
+var commandFunctionMap = {};
+
+function init(){
+    commandMap["clear"] = " - clear command history from screen.";
+
+    commandMap["github"] = " - list Github repositories.";
+
+    commandMap["name"] = " - owner of the résumé.";
+    commandFunctionMap["name"] = name;
+
+    commandMap["pdf"] = " - pdf version of the resume.";
+    commandFunctionMap["pdf"] = pdf;
+
+    commandMap["lookingfor"] = " - looking for.";
+    commandFunctionMap["lookingfor"] = lookingfor;
+
+    commandMap["location"] = " - current location.";
+    commandFunctionMap["location"] = loc;
+
+    commandMap["education"] = " - education history. [-top]";
+    commandFunctionMap["education -top"] = getTop(education);
+    commandFunctionMap["education"] = getAll("Education:\n", education);
+
+    commandMap["employment"] = " - employment history. [-top]";
+    commandFunctionMap["employment -top"] = getTop(employment);
+    commandFunctionMap["employment"] = getAll("Employment:\n", employment);
+
+    commandMap["volunteering"] = " - volunteering history. [-top]";
+    commandFunctionMap["volunteering -top"] = getTop(volunteering);
+    commandFunctionMap["volunteering"] = getAll("Volunteering:\n", volunteering);
+
+    commandMap["awards"] = " - awards obtained.";
+    commandFunctionMap["awards"] = getAll("Awards:\n", awards);
+
+    commandMap["membership"] = " - membership obtained.";
+    commandFunctionMap["membership"] = getAll("Professional membership:\n", membership);
+
+    commandMap["skills"] = " - membership obtained. [-languages|l][-tools|t][-concepts|c]";
+    commandFunctionMap["skills"] = getSkillTable();
+    commandFunctionMap["skills -l"] = commandFunctionMap["skills -language"] 
+        = getAll("Languages:\n", skillsLanguages);
+    commandFunctionMap["skills -t"] = commandFunctionMap["skills -tools"] 
+        = getAll("Tools:\n", skillsTools);
+    commandFunctionMap["skills -c"] = commandFunctionMap["skills -concepts"] 
+        = getAll("Concepts:\n", skillsConcepts);
+
+    commandMap["socialmedia"] = " - Social Media profiles.";
+    commandFunctionMap["socialmedia"] = getSocialMedia();
+
+    commandMap.getKeys = function (){
+    var commands = "Commands:\n";
+    for(var key in this) {
+
+        if( typeof this[key] !== 'function') {
+            commands += key + commandMap[key] + "\n";
+        }
+    }
+    return commands;
+    };
+}
+
 function pdf(){
     window.open(pdfLink);
     return "Hint: May need to allow pop-ups.";
+}
+
+function getAll(result, array){ 
+    array.map( function(item) {
+        result += (item.join()).replaceAll(",","\t") + "\n";
+    });
+    return result;
+}
+
+function getSocialMedia(){
+    var result = "Social Media:\n";
+    socialMedia.map(function(item){
+        if (item[1].length > 0){
+            result += item[0] + " - " + item[1] + "\n";
+        }
+    });
+    return result;
+}
+
+function getRequired(skillList){
+    var result = [];
+    var i = 0;
+    skillList.map(function(item){
+        result[i] = item[1];
+        i++;
+    });
+    return result;
+}
+
+function getSkillTable(){ 
+    var result = "\t\t\t|\tProficient\t|\tExperienced\t|\tFamiliar\n";
+    result += "Languages\t|" + getRequired(skillsLanguages).join().replaceAll(",","\t|\t") + "\n";
+    result += "Tools\t|" + getRequired(skillsTools).join().replaceAll(",","\t|\t") + "\n";
+    result += "Concepts\t|" + getRequired(skillsConcepts).join().replaceAll(",","\t|\t") + "\n";
+
+    return result;
+}
+
+function getTop(array){
+    return (array[0].join()).replaceAll(",","\t");
+}
+
+function social() {
+    
 }
