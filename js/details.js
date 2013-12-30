e22ea43fa862ed69b05fc917a3c834b8ae45c929
Brendon Body <bbody.project@gmail.com>
2013-12-30 16:40:12 +1100

    Added employment info

--- js/details.js
+++ js/details.js
@@ -3,3 +3,4 @@ var name = "Brendon Body";
 var githubUsername = "bbody";
 var bitbucketUsername = "bbody";
 var pdfLink = "https://www.dropbox.com/s/shc3t4ttk0j7xwl/resume.pdf";
+var employment = "Currently interning/Looking for opportunities";
