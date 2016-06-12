b8a1695662951c75a585dbfaeaca7d0c40e31afd
Brendon Body <bbody.project@gmail.com>
2016-06-13 00:46:09 +1000

    Add more test cases

--- testing/js/tests.js
+++ testing/js/tests.js
@@ -1,7 +1,4 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-});
-
+/* --- String Function Tests --- */
 
 QUnit.test( "Test String 'is' function", function( assert ) {
   assert.ok("".is(""));
@@ -16,7 +13,9 @@ QUnit.test( "Test String 'is' function", function( assert ) {
 QUnit.test( "Test String 'capitalizeFirstLetter' function", function( assert ) {
   assert.equal("Hello".capitalizeFirstLetter(), "Hello");
   assert.equal("hello".capitalizeFirstLetter(), "Hello");
+  assert.equal("hELLO".capitalizeFirstLetter(), "HELLO");
   assert.equal("HELLO".capitalizeFirstLetter(), "HELLO");
   assert.equal("".capitalizeFirstLetter(), "");
   assert.equal("z".capitalizeFirstLetter(), "Z");
+  assert.equal("Z".capitalizeFirstLetter(), "Z");
 });
