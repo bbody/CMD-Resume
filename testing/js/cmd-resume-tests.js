39e749be29e59044474a69bd6d84486427c9d234
Brendon Body <bbody.project@gmail.com>
2016-06-16 00:31:31 +1000

    More unit tests

--- testing/js/cmd-resume-tests.js
+++ testing/js/cmd-resume-tests.js
@@ -49,3 +49,67 @@ QUnit.test( "Test looking for", function( assert ) {
 	// TODO: Handle different styles when implemented
 	assert.equal(this.cmdResume.runCommand("lookingfor"), "[[b;green;#000]Raking in the money]");
 });
+
+QUnit.test( "Test looking for", function( assert ) {
+	// Add test when name is null after variables
+	// TODO: Handle different styles when implemented
+	assert.equal(this.cmdResume.runCommand("lookingfor"), "[[b;green;#000]Raking in the money]");
+});
+
+QUnit.test("Test social media", function(assert){
+	// Add test when name is null after variables
+	// TODO: Handle different styles when implemented
+	assert.deepEqual(this.cmdResume.runCommand("socialmedia"), 
+		"[[b;red;#000]Social Media:]\nGithub - https://github.com/example\nBitbucket - https://bitbucket.org/example\nWebsite - http://www.example.com/\nTwitter - https://twitter.com/\nFacebook - https://facebook.com/\nSkype - test");
+});
+
+
+QUnit.test("Test splash screen", function(assert){
+	var expectedResult = ""
+	+ "                 _________________\n"
+	+ "                /                /|\n"
+	+ "               /                / |\n"
+	+ "              /________________/ /|\n"
+	+ "           ###|      ____      |//|\n"
+	+ "          #   |     /   /|     |/.|\n"
+	+ "         #  __|___ /   /.|     |  |_______________\n"
+	+ "        #  /      /   //||     |  /              /|                  ___\n"
+	+ "       #  /      /___// ||     | /              / |                 / \\ \\\n"
+	+ "       # /______/!   || ||_____|/              /  |                /   \\ \\\n"
+	+ "       #| . . .  !   || ||                    /  _________________/     \\ \\\n"
+	+ "       #|  . .   !   || //      ________     /  /\\________________  {   /  }\n"
+	+ "       /|   .    !   ||//~~~~~~/   0000/    /  / / ______________  {   /  /\n"
+	+ "      / |        !   |\'/      /9  0000/    /  / / /             / {   /  /\n"
+	+ "     / #\\________!___|/      /9  0000/    /  / / /_____________/___  /  /\n"
+	+ "    / #     /_____\\/        /9  0000/    /  / / /_  /\\_____________\\/  /\n"
+	+ "   / #                      ``^^^^^^    /   \\ \\ . ./ / ____________   /\n"
+	+ "  +=#==================================/     \\ \\ ./ / /.  .  .  \\ /  /\n"
+	+ "  |#                                   |      \\ \\/ / /___________/  /\n"
+	+ "  #                                    |_______\\__/________________/\n"
+	+ "  |                                    |               |  |  / /       \n"
+	+ "  |                                    |               |  | / /       \n"
+	+ "  |                                    |       ________|  |/ /________       \n"
+	+ "  |                                    |      /_______/    \\_________/\\       \n"
+	+ "  |                                    |     /        /  /           \\ )       \n"
+	+ "  |                                    |    /OO^^^^^^/  / /^^^^^^^^^OO\\)       \n"
+	+ "  |                                    |            /  / /        \n"
+	+ "  |                                    |           /  / /\n"
+	+ "  |                                    |          /___\\/\n"
+	+ "  |hectoras                            |           oo\n"
+	+ "  |____________________________________|\n"
+	+ "\n"
+	+ "Welcome to [[b;green;#000]John Doe]\'s résumé.\n"
+	+ "Type [[i;white;#000]help] for commands";
+
+	// Add test when name is null or disabled after variables
+	// TODO: Handle different styles when implemented
+	assert.deepEqual(this.cmdResume.runCommand("splash"), expectedResult);
+});
+
+QUnit.test("Test pdf", function(assert){
+	// Add test when name is null after variables
+	// TODO: Handle different styles when implemented
+	assert.deepEqual(this.cmdResume.runCommand("pdf"), "http://en.wikipedia.org/wiki/R%C3%A9sum%C3%A9\nHint: May need to allow pop-ups.");
+});
+
+
