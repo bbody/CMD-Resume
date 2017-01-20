aefdf1eb45282690e9414d9fc3d0db426879e7d0
Brendon Body <bbody.project@gmail.com>
2017-01-21 01:46:03 +1100

    Add webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 /* File: gulpfile.js */
 
-// grab our packages
+// Dependancies
 var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
     qunit = require('gulp-qunit'),
@@ -9,14 +9,21 @@ var gulp   = require('gulp'),
 	ghPages = require('gulp-gh-pages'),
 	rename = require('gulp-rename'),
     inject = require('gulp-inject-string'),
-    htmlmin = require('gulp-htmlmin');
+    webserver = require('gulp-webserver');
 
-// define the default task and add the watch task to it
-gulp.task('default', ['watch']);
+// Default Gulp task is develop
+gulp.task('default', ['develop']);
 
-gulp.task('build', ['jshint', 'test', 'compile:js', 'compile:html', 'inject-tag']);
+// Task for development
+gulp.task('develop', ['watch', 'serve'])
 
+// Build the project
+gulp.task('build', ['jshint', 'test', 'compile:js', 'copy:html', 'compile:json', 'copy:icon']);
+
+// Build 
 gulp.task('deploy', ['build', 'gh-pages']);
+
+gulp.task('serve', ['serve:development'])
 
 // configure the jshint task
 gulp.task('jshint', function() {
@@ -45,20 +52,50 @@ gulp.task('compile:js', function (cb) {
   );
 });
 
-gulp.task('compile:html', function() {
+gulp.task('copy:html', function() {
   return gulp.src('index.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+  	.pipe(inject.after('<body>', '<a href="https://github.com/bbody/CMD-Resume"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 100;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>'))
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('copy:json', function() {
+  return gulp.src(['responses/*.json'])
+        .pipe(gulp.dest('dist/responses'));
+});
 
-gulp.task('inject-tag', function(){
-    gulp.src('./dist/index.html')
-        .pipe(inject.after('<body>', '<a href="https://github.com/bbody/CMD-Resume"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 100;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>'))
-        .pipe(gulp.dest('dist'));
+gulp.task('copy:icon', function(){
+	return gulp.src('favicon.ico')
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve:development', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      fallback: 'index.html'
+    }));
+});
+
+gulp.task('serve:test', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      fallback: 'spec/index.html'
+    }));
+});
+
+gulp.task('serve:staging', function() {
+  gulp.src('./dist')
+    .pipe(webserver({
+      livereload: true,
+      open: true,
+      fallback: 'index.html'
+    }));
 });
 
 gulp.task('gh-pages', function() {
-  return gulp.src('./dist/**/*')
+  return gulp.src(['dist/**/*', 'dist/*'])
     .pipe(ghPages());
 });
