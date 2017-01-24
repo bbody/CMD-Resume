b7aebab75cc3ae2ca287f027d4b198cf2d30fe5f
Brendon Body <bbody.project@gmail.com>
2017-01-24 22:43:49 +1100

    Recompile

--- gulpfile.js
+++ gulpfile.js
@@ -19,16 +19,20 @@ var gulp   = require('gulp'),
 gulp.task('default', ['develop']);
 
 // Task for development
-gulp.task('develop', ['watch', 'serve'])
+gulp.task('develop', ['watch', 'build', 'serve']);
 
 // Build the project
-gulp.task('build', ['jshint', 'test', 'compile:js', 'copy:html', 'copy:json', 'copy:icon']);
+gulp.task('build', ['jshint', 'test', 'compile:development', 'copy:html', 'copy:json', 'copy:icon']);
+
+gulp.task('build-gh-pages', ['jshint', 'test', 'compile:gh-pages', 'copy:html', 'copy:json', 'copy:icon']);
 
 // Build then deploy to Github Pages
-gulp.task('deploy', ['build', 'gh-pages']);
+gulp.task('deploy', ['build-gh-pages', 'gh-pages']);
 
 // Serve the development copy
-gulp.task('serve', ['serve:development'])
+gulp.task('serve', ['serve:development']);
+
+gulp.task('release', ['compile:release:minified', 'compile:release', 'zip']);
 
 // JSHint the JavaScript files
 gulp.task('jshint', function() {
@@ -53,7 +57,7 @@ gulp.task('compile:js', function (cb) {
   pump([
         gulp.src('js/*.js'),
         uglify(),
-        gulp.dest('dist/js')
+        gulp.dest('tmp/js')
     ],
     cb
   );
@@ -63,24 +67,24 @@ gulp.task('compile:js', function (cb) {
 gulp.task('copy:html', function() {
   return gulp.src('index.html')
   	.pipe(inject.after('<body>', '<a href="https://github.com/bbody/CMD-Resume"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 100;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>'))
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('tmp'));
 });
 
-// Copy JSON files to dist
+// Copy JSON files to tmp
 gulp.task('copy:json', function() {
   return gulp.src(['responses/*.json'])
-        .pipe(gulp.dest('dist/responses'));
+        .pipe(gulp.dest('tmp/responses'));
 });
 
-// Copy favicon to dist
+// Copy favicon to tmp
 gulp.task('copy:icon', function(){
 	return gulp.src('favicon.ico')
-		.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('tmp'));
 });
 
 // Serve the for development
 gulp.task('serve:development', function() {
-  gulp.src('./')
+  gulp.src('./tmp')
     .pipe(webserver({
       livereload: true,
       open: true,
@@ -98,19 +102,9 @@ gulp.task('serve:test', function() {
     }));
 });
 
-// Serve the dist environment
-gulp.task('serve:dist', function() {
-  gulp.src('./dist')
-    .pipe(webserver({
-      livereload: true,
-      open: true,
-      fallback: 'index.html'
-    }));
-});
-
 // Push dist to Github Pages
 gulp.task('gh-pages', function() {
-  return gulp.src(['dist/**/*', 'dist/*'])
+  return gulp.src(['tmp/**/*', 'tmp/*'])
     .pipe(ghPages());
 });
 
@@ -118,30 +112,57 @@ gulp.task('version', function(){
 	return getVersion();
 });
 
-gulp.task('release', function(){
-	var versionInfo = '/* v' + getVersion() + ' of CMD Resume by Brendon Body */';
-	
-  	gulp.src('js/cmd-resume.js')
+gulp.task('compile:release:minified', function(){
+	return gulp.src('js/cmd-resume.js')
   		.pipe(uglify())
   		.pipe(rename("cmd-resume.min.js"))
   		.pipe(inject.prepend("\"use strict\";"))
   		.pipe(inject.prepend("(function($){"))
-  		.pipe(inject.prepend(versionInfo))
+  		.pipe(inject.prepend(getVersionString()))
   		.pipe(inject.append("}(jQuery));"))
   		.pipe(gulp.dest('dist'));
+});
 
-  	gulp.src('js/cmd-resume.js')
+gulp.task('compile:release', function(){
+	return gulp.src('js/cmd-resume.js')
 		.pipe(inject.prepend("\n\"use strict\";\n\n"))
   		.pipe(inject.afterEach("\n", "	"))
   		.pipe(inject.prepend("(function($){"))
-  		.pipe(inject.prepend(versionInfo + "\n\n"))
+  		.pipe(inject.prepend(getVersionString() + "\n\n"))
   		.pipe(inject.append("\n}(jQuery));"))
     	.pipe(gulp.dest('dist'));
+});
 
-	gulp.src('dist/*.js')
+gulp.task('compile:development', function(){
+	return gulp.src('js/cmd-resume.js')
+		.pipe(inject.prepend("\n\"use strict\";\n\n"))
+  		.pipe(inject.afterEach("\n", "	"))
+  		.pipe(inject.prepend("(function($){"))
+  		.pipe(inject.prepend(getVersionString() + "\n\n"))
+  		.pipe(inject.append("\n}(jQuery));"))
+    	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('compile:gh-pages', function(){
+	return gulp.src('js/cmd-resume.js')
+		.pipe(inject.prepend("\n\"use strict\";\n\n"))
+  		.pipe(inject.afterEach("\n", "	"))
+  		.pipe(inject.prepend("(function($){"))
+  		.pipe(inject.prepend(getVersionString() + "\n\n"))
+  		.pipe(inject.append("\n}(jQuery));"))
+    	.pipe(gulp.dest('tmp/js'));
+});
+
+gulp.task('zip', function(){
+	return gulp.src('dist/*.js')
         .pipe(zip('release-v' + getVersion() +'.zip'))
         .pipe(gulp.dest('dist'));
 });
+
+function getVersionString(){
+	var versionInfo = '/* v' + getVersion() + ' of CMD Resume by Brendon Body */';
+	return versionInfo;
+}
 
 function getVersion(){
 	var json = JSON.parse(fs.readFileSync('./package.json'));
