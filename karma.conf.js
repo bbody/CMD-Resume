e4e110d25c9b324e5a354285497ecb454e3ec177
Brendon Body <bbody.project@gmail.com>
2017-06-17 03:18:59 +1000

    Add coverage generator to Karmacode

--- karma.conf.js
+++ karma.conf.js
@@ -67,6 +67,11 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
   })
 }
