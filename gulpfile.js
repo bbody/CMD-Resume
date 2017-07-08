80c50782577c2b4e259f090024cdbfa66f09bf3c
Brendon Body <bbody.project@gmail.com>
2017-07-08 17:59:43 +1000

    Update tools with style changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@
 
 // Dependancies
 var gulp   = require('gulp'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    inject = require('gulp-inject-string'),
-    webserver = require('gulp-webserver'),
-    fs = require('fs'),
-    concat = require('gulp-concat'),
-    jscs = require('gulp-jscs'),
-    Server = require('karma').Server;
+		jshint = require('gulp-jshint'),
+		uglify = require('gulp-uglify'),
+		inject = require('gulp-inject-string'),
+		webserver = require('gulp-webserver'),
+		fs = require('fs'),
+		concat = require('gulp-concat'),
+		jscs = require('gulp-jscs'),
+		Server = require('karma').Server;
 
 // Default Gulp task is develop
 gulp.task('default', ['develop']);
@@ -21,118 +21,129 @@ gulp.task('develop', ['watch', 'build', 'serve']);
 gulp.task('test', ['watch', 'build', 'test:karma', 'coverage']);
 
 // Build the project
-gulp.task('build', ['jscs:development', 'test:karma', 'compile:development', 'jshint:development', 'copy:html', 'copy:json', 'copy:icon']);
+gulp.task('build', ['jscs:development', 'test:karma', 'compile:development',
+	'jshint:development', 'copy:html', 'copy:json', 'copy:icon']);
 
 gulp.task('release', ['compile:release:minified', 'compile:release']);
 
 // Watch important files
 gulp.task('watch', function() {
-  gulp.watch(['js/*.js', 'index.html', 'spec/*.js', 'karma.conf.js'], ['jshint:development', 'test:karma']);
+	gulp.watch(['js/*.js', 'index.html', 'spec/*.js', 'karma.conf.js'],
+		['jshint:development', 'test:karma']);
 });
 
 // Serve the for development
 gulp.task('serve', function() {
-  gulp.src('./tmp/')
-    .pipe(webserver({
-      livereload: true,
-      open: true,
-      fallback: 'index.html'
-    }));
+	gulp.src('./tmp/')
+		.pipe(webserver({
+			livereload: true,
+			open: true,
+			fallback: 'index.html'
+		}));
 });
 
 // Source code checking
 gulp.task('jshint:development', function() {
-  return gulp.src(['./tmp/js/cmd-resume.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'));
+	return gulp.src(['./tmp/js/cmd-resume.js'])
+		.pipe(jshint('./js/'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(jshint.reporter('fail'));
 });
 
-gulp.task('jscs:development', function(){
-  return gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
-    .pipe(jscs())
-    .pipe(jscs.reporter())
-    .pipe(jscs.reporter('fail'));
+gulp.task('jscs:development', function() {
+	return gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
+		.pipe(jscs())
+		.pipe(jscs.reporter())
+		.pipe(jscs.reporter('fail'));
 });
 
-gulp.task('jscs', function(){
-  return gulp.src(['dist/cmd-resume.js'])
-    .pipe(jscs({fix: true}))
-    .pipe(jscs.reporter())
-    .pipe(gulp.dest('tmp'));
+gulp.task('jscs:tools', function() {
+	return gulp.src(['karma.conf.js', 'gulpfile.js'])
+		.pipe(jscs())
+		.pipe(jscs.reporter())
+		.pipe(jscs.reporter());
+});
+
+gulp.task('jscs:tests', function() {
+	return gulp.src(['spec/*-spec.js'])
+		.pipe(jscs('./spec'))
+		.pipe(jscs.reporter())
+		.pipe(jscs.reporter());
 });
 
 // Copy HTML across (Also inject Github ribbon)
 gulp.task('copy:html', function() {
-  return gulp.src('index.html')
-  	.pipe(inject.after('<body>', '<a href="https://github.com/bbody/CMD-Resume"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 100;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>'))
-    .pipe(gulp.dest('tmp'));
+	// Ignoring length as the string has no logic and would be neater on one line
+	// jscs:disable maximumLineLength
+	return gulp.src('index.html')
+		.pipe(inject.after('<body>', '<a href="https://github.com/bbody/CMD-Resume"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 100;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>'))
+		.pipe(gulp.dest('tmp'));
+	// jscs:enable maximumLineLength
 });
 
 // Copy JSON files to tmp
 gulp.task('copy:json', function() {
-  return gulp.src(['responses/*.json'])
-        .pipe(gulp.dest('tmp/responses'));
+	return gulp.src(['responses/*.json'])
+				.pipe(gulp.dest('tmp/responses'));
 });
 
 // Copy favicon to tmp
-gulp.task('copy:icon', function(){
+gulp.task('copy:icon', function() {
 	return gulp.src('favicon.ico')
 		.pipe(gulp.dest('tmp'));
 });
 
 // Compile JavaScript
-gulp.task('compile:release:minified', function(){
-  return compiledCode('./dist', true, true);
+gulp.task('compile:release:minified', function() {
+	return compiledCode('./dist', true, true);
 });
 
-gulp.task('compile:release', function(){
-  return compiledCode('./dist', false, true);
+gulp.task('compile:release', function() {
+	return compiledCode('./dist', false, true);
 });
 
-gulp.task('compile:development', function(){
-  return compiledCode('./tmp/js', false, false);
+gulp.task('compile:development', function() {
+	return compiledCode('./tmp/js', false, false);
 });
 
 // Testing
-gulp.task('test:karma', function(done){
-  return new Server({
-    configFile: __dirname + '/karma.conf.js',
-    singleRun: true
-  }, done).start();
+gulp.task('test:karma', function(done) {
+	return new Server({
+		configFile: __dirname + '/karma.conf.js',
+		singleRun: true
+	}, done).start();
 });
 
 gulp.task('test:e2e', function() {
-    return gulp.src('wdio.conf.js').pipe(webdriver());
+	return gulp.src('wdio.conf.js').pipe(webdriver());
 });
 
 // Useful functions
-function getVersionString(){
+function getVersionString() {
 	var versionInfo = '/* v' + getVersion() + ' of CMD Resume by Brendon Body */';
 	return versionInfo;
 }
 
-function getVersion(){
+function getVersion() {
 	var json = JSON.parse(fs.readFileSync('./package.json'));
 	return json.version;
 }
 
-function compiledCode(destination, minified, versioned){
-  var stream =  gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
-    .pipe(concat(minified ? 'cmd-resume.min.js' : 'cmd-resume.js'));
+function compiledCode(destination, minified, versioned) {
+	var stream =  gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
+		.pipe(concat(minified ? 'cmd-resume.min.js' : 'cmd-resume.js'));
 
-  stream.pipe(inject.prepend(";(function($){\n\"use strict\";\n\n"))
-    .pipe(inject.afterEach("\n", "  "))
-    .pipe(inject.append("\n}(jQuery));"));
-    
+	stream.pipe(inject.prepend(';(function($){\n"use strict";\n\n'))
+		.pipe(inject.afterEach('\n', '  '))
+		.pipe(inject.append('\n}(jQuery));'));
 
-  if (versioned){
-    stream.pipe(inject.prepend(getVersionString() + "\n"));
-  }
+	if (versioned) {
+		stream.pipe(inject.prepend(getVersionString() + '\n'));
+	}
 
-  if (minified){
-    stream.pipe(uglify());
-  }
+	if (minified) {
+		stream.pipe(uglify());
+	}
 
-  return stream.pipe(gulp.dest(destination));
+	return stream.pipe(gulp.dest(destination));
 }
--- karma.conf.js
+++ karma.conf.js
@@ -1,81 +1,33 @@
 // Karma configuration
-// Generated on Fri Jun 16 2017 23:37:42 GMT+1000 (AEST)
-
 module.exports = function(config) {
 	config.set({
-
-		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: '',
-
-
-		// frameworks to use
-		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		// Frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ['jasmine-ajax', 'jasmine', 'jquery-3.1.1'],
-
-
-		// list of files / patterns to load in the browser
 		files: [
 			'node_modules/jquery.terminal/js/jquery.terminal.js',
 			'js/*.js',
 			'spec/helping-functions-spec.js',
 			// 'spec/cmd-resume-spec.js'
 		],
-
-
-		// list of files to exclude
 		exclude: [],
-
-
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
 			'js/*.js': ['coverage']
 		},
-
-
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['progress', 'coverage'],
-
-
-		// web server port
 		port: 9876,
-
-
-		// enable / disable colors in the output (reporters and logs)
 		colors: true,
-
-
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
-
-
-		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: true,
-
-
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		// Available browsers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['PhantomJS' /*,'Chrome', 'Firefox', 'Safari' */],
-
-
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
 		singleRun: false,
-
-		// Concurrency level
-		// how many browser should be started simultaneous
 		concurrency: Infinity,
-
 		coverageReporter: {
 			reporters: [
-                // generates ./coverage/lcov.info
-                {type:'lcovonly', subdir: '.'},
-                // generates ./coverage/coverage-final.json
-                {type:'json', subdir: '.'}
-            ]
+				{type: 'lcovonly', subdir: '.'},
+				{type: 'json', subdir: '.'}
+			]
 		}
-	})
-}
+	});
+};
