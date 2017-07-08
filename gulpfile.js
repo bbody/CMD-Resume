96584b4e46956a7ab1471c67500d925d21e7d12f
Brendon Body <bbody.project@gmail.com>
2017-07-08 20:36:37 +1000

    :rocket: Refactor tasks and add source checks for tools and tests

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,41 @@ var gulp   = require('gulp'),
 		jscs = require('gulp-jscs'),
 		Server = require('karma').Server;
 
+var TOOLS = ['karma.conf.js', 'gulpfile.js'];
+var TESTS = ['spec/*-spec.js'];
+var SOURCE = ['js/helper-functions.js', 'js/cmd-resume.js'];
+var OUTPUT = ['./tmp/js/cmd-resume.js'];
+
+// Useful functions
+function getVersion() {
+	var json = JSON.parse(fs.readFileSync('./package.json'));
+	return json.version;
+}
+
+function getVersionString() {
+	var versionInfo = '/* v' + getVersion() + ' of CMD Resume by Brendon Body */';
+	return versionInfo;
+}
+
+function compiledCode(destination, minified, versioned) {
+	var stream =  gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
+		.pipe(concat(minified ? 'cmd-resume.min.js' : 'cmd-resume.js'));
+
+	stream.pipe(inject.prepend(';(function($){\n"use strict";\n\n'))
+		.pipe(inject.afterEach('\n', '  '))
+		.pipe(inject.append('\n}(jQuery));'));
+
+	if (versioned) {
+		stream.pipe(inject.prepend(getVersionString() + '\n'));
+	}
+
+	if (minified) {
+		stream.pipe(uglify());
+	}
+
+	return stream.pipe(gulp.dest(destination));
+}
+
 // Default Gulp task is develop
 gulp.task('default', ['develop']);
 
@@ -21,15 +56,16 @@ gulp.task('develop', ['watch', 'build', 'serve']);
 gulp.task('test', ['watch', 'build', 'test:karma', 'coverage']);
 
 // Build the project
-gulp.task('build', ['jscs:development', 'test:karma', 'compile:development',
-	'jshint:development', 'copy:html', 'copy:json', 'copy:icon']);
+gulp.task('build', ['source-check:development', 'source-check:tests',
+	'source-check:tools', 'test:karma', 'compile:development', 'copy:html',
+	'copy:json', 'copy:icon']);
 
 gulp.task('release', ['compile:release:minified', 'compile:release']);
 
 // Watch important files
 gulp.task('watch', function() {
 	gulp.watch(['js/*.js', 'index.html', 'spec/*.js', 'karma.conf.js'],
-		['jshint:development', 'test:karma']);
+		['build']);
 });
 
 // Serve the for development
@@ -43,30 +79,48 @@ gulp.task('serve', function() {
 });
 
 // Source code checking
+gulp.task('source-check:development', ['jshint:development', 'jscs:development']);
+gulp.task('source-check:tools', ['jshint:tools', 'jscs:tools']);
+gulp.task('source-check:tests', ['jshint:tests', 'jscs:tests']);
+
 gulp.task('jshint:development', function() {
-	return gulp.src(['./tmp/js/cmd-resume.js'])
-		.pipe(jshint('./js/'))
+	return gulp.src(OUTPUT)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jshint:tools', function() {
+	return gulp.src(TOOLS)
+		.pipe(jshint('./.jshintrc-tools'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jshint:tests', function() {
+	return gulp.src(TESTS)
+		.pipe(jshint('./.jshintrc-tests'))
 		.pipe(jshint.reporter('jshint-stylish'))
 		.pipe(jshint.reporter('fail'));
 });
 
 gulp.task('jscs:development', function() {
-	return gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
+	return gulp.src(SOURCE)
 		.pipe(jscs())
 		.pipe(jscs.reporter())
 		.pipe(jscs.reporter('fail'));
 });
 
 gulp.task('jscs:tools', function() {
-	return gulp.src(['karma.conf.js', 'gulpfile.js'])
-		.pipe(jscs())
+	return gulp.src(TOOLS)
+		.pipe(jscs({configPath: './.jscsrc-tools'}))
 		.pipe(jscs.reporter())
 		.pipe(jscs.reporter());
 });
 
 gulp.task('jscs:tests', function() {
-	return gulp.src(['spec/*-spec.js'])
-		.pipe(jscs('./spec'))
+	return gulp.src(TESTS)
+		.pipe(jscs({configPath: './spec/.jscsrc'}))
 		.pipe(jscs.reporter())
 		.pipe(jscs.reporter());
 });
@@ -74,11 +128,11 @@ gulp.task('jscs:tests', function() {
 // Copy HTML across (Also inject Github ribbon)
 gulp.task('copy:html', function() {
 	// Ignoring length as the string has no logic and would be neater on one line
-	// jscs:disable maximumLineLength
 	return gulp.src('index.html')
+		// jscs:disable maximumLineLength
 		.pipe(inject.after('<body>', '<a href="https://github.com/bbody/CMD-Resume"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 100;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>'))
+		// jscs:enable maximumLineLength
 		.pipe(gulp.dest('tmp'));
-	// jscs:enable maximumLineLength
 });
 
 // Copy JSON files to tmp
@@ -115,35 +169,5 @@ gulp.task('test:karma', function(done) {
 });
 
 gulp.task('test:e2e', function() {
-	return gulp.src('wdio.conf.js').pipe(webdriver());
+	// return gulp.src('wdio.conf.js').pipe(webdriver());
 });
-
-// Useful functions
-function getVersionString() {
-	var versionInfo = '/* v' + getVersion() + ' of CMD Resume by Brendon Body */';
-	return versionInfo;
-}
-
-function getVersion() {
-	var json = JSON.parse(fs.readFileSync('./package.json'));
-	return json.version;
-}
-
-function compiledCode(destination, minified, versioned) {
-	var stream =  gulp.src(['js/helper-functions.js', 'js/cmd-resume.js'])
-		.pipe(concat(minified ? 'cmd-resume.min.js' : 'cmd-resume.js'));
-
-	stream.pipe(inject.prepend(';(function($){\n"use strict";\n\n'))
-		.pipe(inject.afterEach('\n', '  '))
-		.pipe(inject.append('\n}(jQuery));'));
-
-	if (versioned) {
-		stream.pipe(inject.prepend(getVersionString() + '\n'));
-	}
-
-	if (minified) {
-		stream.pipe(uglify());
-	}
-
-	return stream.pipe(gulp.dest(destination));
-}
