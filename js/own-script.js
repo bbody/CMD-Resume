d546435db743727e6c90c50c825a99936ec016b6
Brendon Body <bbody.project@gmail.com>
2017-07-09 23:45:30 +1000

    Update URI to endpoint

--- js/own-script.js
+++ js/own-script.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     var settings = {
         showForks: true
     };
-    $("body").CMDResume("https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json", "../responses/extra-details.json", settings);
+    
+    $("body").CMDResume("https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json", "../responses/my-extra-details.json", settings);
 });
