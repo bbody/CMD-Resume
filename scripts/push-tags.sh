9064271c6d8512b6183a33ba25a612b64917401d
Brendon Body <bbody.project@gmail.com>
2017-07-22 19:26:59 +1000

    :rocket::pencil: NPM release process (#122)
    * :pencil: 3.0.70
    
    * :pencil: 3.0.71
    
    * :pencil: 3.0.72
    
    * :rocket: Add npm version bumping
    
    * :newspaper::rocket: Add an ignore file for NPM releasing
    
    * :rocket: Add publish npm statement
    
    * :rocket: Add NPM provider
    
    * :pencil: Update npm ignore
    
    * :pencil: Move files
    
    * :pencil::rocket: Move script out of Travis config
    
    * :pencil: Make script executable
    
    * :rocket::pencil: Fix script

---
+++ scripts/push-tags.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create a git tag of the new version to use
+# If package.json major and minor versions match last tag, then increment last tag. Else use package.json major.minor.0.
+"{ sed -nE 's/^[ \\t]*\"version\": \"([0-9]{1,}\\.[0-9]{1,}\\.)[0-9x]{1,}\",$/\\1/p' package.json; git describe --abbrev=0 | sed -E 's/^v([0-9]{1,}\\.[0-9]{1,}\\.)([0-9]{1,})$/\\1 \\2/g'; } | tr \"\\n\" \" \" | awk '{printf($1==$2?\"v\"$2$3+1:\"v\"$1\"0\")}' | xargs -I {} git tag -a {} -m \"{}\"\n"
+
+# Update package.json based on the git tag we just created
+npm --no-git-tag-version version from-git
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote rm origin
+git remote add origin https://bbody:${GITHUB_TOKEN}@github.com/bbody/CMD-Resume.git
+git push --tags
