a256478f4fba34bf2d11b720a52dd1316f558186
Brendon Body <bbody.project@gmail.com>
2017-07-22 19:59:12 +1000

    :rocket: NPM update repo (#124)
    * :pencil: 3.0.70
    
    * :pencil: 3.0.71
    
    * :pencil: 3.0.72
    
    * :rocket: Add npm version bumping
    
    * :newspaper::rocket: Add an ignore file for NPM releasing
    
    * :rocket: Add publish npm statement
    
    * :rocket: Add NPM provider
    
    * :pencil: Update npm ignore
    
    * :pencil: Move files
    
    * :pencil::rocket: Move script out of Travis config
    
    * :pencil: Make script executable
    
    * :rocket::pencil: Fix script
    
    * :newspaper: Add package lock
    
    * :pencil: Push changes to master

--- scripts/push-tags.sh
+++ scripts/push-tags.sh
@@ -11,4 +11,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote rm origin
 git remote add origin https://bbody:${GITHUB_TOKEN}@github.com/bbody/CMD-Resume.git
+git add package.json
+git add package-lock.json
+git push origin master
 git push --tags
