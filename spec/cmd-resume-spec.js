b04e1e61acece0479455e2831ebbcc7e0d6e447a
Brendon Body <bbody.project@gmail.com>
2018-05-28 00:51:59 +1000

    :umbrella: Add tests for basic commands

--- spec/cmd-resume-spec.js
+++ spec/cmd-resume-spec.js
@@ -2,28 +2,28 @@ describe("CMDResume Plugin", function() {
 	beforeEach(function() {
 		var div = $("<div id='cmd-resume'></div>");
 		$("body").append(div);
+		jasmine.Ajax.install();
 	});
 
 	afterEach(function() {
 		$("#cmd-resume").remove();
+		jasmine.Ajax.uninstall();
 	});
 
 	describe("Splash screen", function() {
 		describe("Featuring name", function() {
 			beforeEach(function() {
-				spyOn($, "getJSON").and.callFake(function(url, success) {
-					// console.log(loadJSON('justName'));
-					success(loadJSON('justName'));
-
-					return {
-						fail: () => {}
-					};
-				});
-
 				$("#cmd-resume").CMDResume("justName.json", {});
 			});
 
 			it("Includes the name in splash", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("justName"))
+				});
+
 				var splash = getSplash();
 
 				expect(splash.intro).toEqual("Welcome to ");
@@ -34,18 +34,259 @@ describe("CMDResume Plugin", function() {
 
 		describe("Without name", function() {
 			beforeEach(function() {
-				spyOn($, "getJSON").and.callFake(function(url, success) {
-					success(loadJSON('empty'));
-					return {
-						fail: () => {}
-					};
-				});
 				$("#cmd-resume").CMDResume("noName.json", {});
 			});
 
 			it("Includes the basic splash", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
+
 				var splash = getSimpleSplash();
 				expect(splash).toEqual("Welcome to my résumé.");
+			});
+		});
+
+		describe("Empty name", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+			});
+
+			it("Includes the basic splash", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("empty"))
+				});
+
+				var splash = getSimpleSplash();
+				expect(splash).toEqual("Welcome to my résumé.");
+			});
+		});
+
+		// describe("Custom splash", function() {
+		// 	beforeEach(function() {
+		// 		$("#cmd-resume").CMDResume("noName",
+		// 			{extraDetails: "customSplash.json"});
+		// 	});
+
+		// 	it("Includes the basic splash", function() {
+		// 		jasmine.Ajax.stubRequest("noName.json").andReturn({
+		// 			status: 200,
+		// 		    responseText: {basics: {name: "Someoneone"}}
+		// 		});
+
+		// 		jasmine.Ajax.stubRequest("customSplash.json").andReturn({
+		// 			status: 200,
+		// 		    responseText: loadJSON("customSplash")
+		// 		});
+
+		// 		jasmine.Ajax.stubRequest("noName").andReturn({
+		// 			status: 200,
+		// 			"responseText": JSON.stringify(loadJSON("noName"))
+		// 		});
+
+		// 		var splash = getSimpleSplash();
+		// 		expect(splash).toEqual("Custom splash");
+		// 	});
+		// });
+	});
+
+	describe("Name", function() {
+		describe("Featuring name", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("justName.json", {});
+			});
+
+			it("Includes the name", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("justName"))
+				});
+
+				enterCommand("name");
+
+				var output = getSimpleOutput();
+				expect(output.summary).toEqual("Name");
+				expect(output.value).toEqual("Richard Hendriks");
+			});
+		});
+
+		describe("Without name", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+			});
+
+			it("Returns command error", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("empty"))
+				});
+
+				enterCommand("name");
+
+				var output = failedCommandOutput();
+
+				expect(output.command).toEqual("name");
+				expect(output.message).toEqual(" is an unknown command.");
+			});
+		});
+
+		describe("Empty name", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+				jasmine.Ajax.requests.mostRecent().respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
+			});
+
+			it("Returns command error", function() {
+
+				enterCommand("name");
+
+				var output = failedCommandOutput();
+
+				expect(output.command).toEqual("name");
+				expect(output.message).toEqual(" is an unknown command.");
+			});
+		});
+	});
+
+	describe("About", function() {
+		describe("Featuring about", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("about.json", {});
+			});
+
+			it("Includes about", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("about"))
+				});
+
+				enterCommand("about");
+
+				var output = getSimpleOutput();
+				expect(output.summary).toEqual("About");
+				expect(output.value).toEqual("Some blurb");
+			});
+		});
+
+		describe("Without about", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+			});
+
+			it("Returns command error", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("empty"))
+				});
+
+				enterCommand("about");
+
+				var output = failedCommandOutput();
+
+				expect(output.command).toEqual("about");
+				expect(output.message).toEqual(" is an unknown command.");
+			});
+		});
+
+		describe("Empty about", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+				jasmine.Ajax.requests.mostRecent().respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
+			});
+
+			it("Returns command error", function() {
+
+				enterCommand("about");
+
+				var output = failedCommandOutput();
+
+				expect(output.command).toEqual("about");
+				expect(output.message).toEqual(" is an unknown command.");
+			});
+		});
+	});
+
+	describe("Location", function() {
+		describe("Featuring location", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("about.json", {});
+			});
+
+			it("Includes about", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("fullExample"))
+				});
+
+				enterCommand("location");
+
+				var output = getSimpleOutput();
+				expect(output.summary).toEqual("Location");
+				expect(output.value).toEqual("San Francisco, California, US");
+			});
+		});
+
+		describe("Without content", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+			});
+
+			it("Returns command error", function() {
+				var mostRecentRequest = jasmine.Ajax.requests.mostRecent();
+
+				mostRecentRequest.respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("empty"))
+				});
+
+				enterCommand("location");
+
+				var output = failedCommandOutput();
+
+				expect(output.command).toEqual("location");
+				expect(output.message).toEqual(" is an unknown command.");
+			});
+		});
+
+		describe("Empty string", function() {
+			beforeEach(function() {
+				$("#cmd-resume").CMDResume("noName.json", {});
+				jasmine.Ajax.requests.mostRecent().respondWith({
+					status: 200,
+					responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
+			});
+
+			it("Returns command error", function() {
+
+				enterCommand("location");
+
+				var output = failedCommandOutput();
+
+				expect(output.command).toEqual("location");
+				expect(output.message).toEqual(" is an unknown command.");
 			});
 		});
 	});
--- spec/support/helpers.js
+++ spec/support/helpers.js
@@ -1,15 +1,35 @@
 var getSplash = function() {
-	var splash = $('.terminal-output div[data-index="0"] span');
+	var splash = $(".terminal-output div[data-index='0'] span");
 	return {
-		intro: $(splash[0]).html().decodeSpace(),
-		name: $(splash[1]).html().decodeSpace(),
-		end: $(splash[2]).html().decodeSpace()
+		intro: $(splash[0]) && $(splash[0]).html() ? $(splash[0]).html().decodeSpace() : "",
+		name: $(splash[1]) && $(splash[1]).html() ? $(splash[1]).html().decodeSpace() : "",
+		end: $(splash[2]) && $(splash[2]).html() ? $(splash[2]).html().decodeSpace() : ""
 	};
 };
 
 var getSimpleSplash = function() {
-	var splash = $('.terminal-output div[data-index="0"] span');
+	var splash = $(".terminal-output div[data-index='0'] span");
 	return $(splash[0]).html().decodeSpace();
+};
+
+var getSimpleOutput =  function() {
+	var lines = $(".terminal-output > div");
+	var output = $(lines[lines.length - 1]).find("div span");
+
+	return {
+		summary: $(output[0]).html().decodeSpace(),
+		value: $(output[1]).html().decodeSpace()
+	};
+};
+
+var failedCommandOutput = function() {
+	var lines = $(".terminal-output > div");
+	var output = $(lines[lines.length - 1]).find("div span");
+
+	return {
+		command: $(output[0]).html().decodeSpace(),
+		message: $(output[1]).html().decodeSpace()
+	};
 };
 
 String.prototype.decodeSpace = function() {
@@ -18,4 +38,67 @@ String.prototype.decodeSpace = function() {
 
 var loadJSON = function(fixtureName) {
 	return window.__json__[fixtureName];
+};
+
+var spyJSON = function(fixtureName, extraDetailsFixtureName = null) {
+	var spy = spyOn($, "getJSON");
+	spy.and.callFake(function(url, success) {
+		if (extraDetailsFixtureName && url === `${extraDetailsFixtureName}.json`){
+			success(loadJSON(`extraDetails/${extraDetailsFixtureName}`));
+		} else {
+			success(loadJSON(fixtureName));
+		}
+
+		return {
+			fail: function() {}
+		};
+	});
+	return spy;
+};
+
+var typeCharacters = function(text) {
+	// Based off enter_text https://github.com/jcubic/jquery.terminal/blob/master/spec/terminalSpec.js
+	var docElement = $(document.documentElement || window);
+
+	text.split("").forEach(function(character){
+		var event = $.Event("keydown");
+		event.keyCode = character.toUpperCase().charCodeAt(0);
+		event.which = character.toUpperCase().charCodeAt(0);
+		event.key = character;
+		docElement.trigger(event);
+
+		event = $.Event("keypress");
+		event.keyCode = character.charCodeAt(0);
+		event.which = character.charCodeAt(0);
+		event.key = character;
+		event.ctrlKey = false;
+		event.altKey = false;
+		docElement.trigger(event);		
+	});
+};
+
+var pressEnter = function() {
+	var ENTER_KEY = 13;
+	// Based off enter https://github.com/jcubic/jquery.terminal/blob/master/spec/terminalSpec.js
+	var docElement = $(document.documentElement || window);
+
+	var event = $.Event("keydown");
+    event.ctrlKey = false;
+    event.key = "enter";
+    event.altKey = false;
+    event.shiftKey = false;
+    event.which = ENTER_KEY;
+    event.keyCode = ENTER_KEY;
+    docElement.trigger(event);
+
+    event = $.Event("keypress");
+    event.key = "enter";
+    event.which = 0;
+    event.keyCode = 0;
+    docElement.trigger(event);
+};
+
+var enterCommand = function(command) {
+	typeCharacters(command);
+	pressEnter();
 };
