24beb91e10375d625aafd8e7bae90ca6a6e07e07
Brendon Body <bbody.project@gmail.com>
2018-05-27 16:58:57 +1000

    :umbrella: Add tests for plugin code

--- spec/cmd-resume-spec.js
+++ spec/cmd-resume-spec.js
@@ -1,10 +1,52 @@
 describe("CMDResume Plugin", function() {
 	beforeEach(function() {
-		this.$div = $(document.createElement("div"));
-		this.term = this.$div.CMDResume("");
+		var div = $("<div id='cmd-resume'></div>");
+		$("body").append(div);
 	});
 
-	it("", function() {
-		expect(this.term.CMDResume.commands).toEqual("");
+	afterEach(function() {
+		$("#cmd-resume").remove();
+	});
+
+	describe("Splash screen", function() {
+		describe("Featuring name", function() {
+			beforeEach(function() {
+				spyOn($, "getJSON").and.callFake(function(url, success) {
+					// console.log(loadJSON('justName'));
+					success(loadJSON('justName'));
+
+					return {
+						fail: () => {}
+					};
+				});
+
+				$("#cmd-resume").CMDResume("justName.json", {});
+			});
+
+			it("Includes the name in splash", function() {
+				var splash = getSplash();
+
+				expect(splash.intro).toEqual("Welcome to ");
+				expect(splash.name).toEqual("Richard Hendriks");
+				expect(splash.end).toEqual("'s résumé.");
+			});
+		});
+
+		describe("Without name", function() {
+			beforeEach(function() {
+				spyOn($, "getJSON").and.callFake(function(url, success) {
+					success(loadJSON('empty'));
+					return {
+						fail: () => {}
+					};
+				});
+				$("#cmd-resume").CMDResume("noName.json", {});
+			});
+
+			it("Includes the basic splash", function() {
+				var splash = getSimpleSplash();
+				expect(splash).toEqual("Welcome to my résumé.");
+			});
+		});
 	});
 });
