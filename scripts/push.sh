309d9eed843b2bb90b1504e6c106be7b615686e5
Brendon Body <bbody.project@gmail.com>
2018-06-04 04:26:26 +1000

    :rocket: Put in token

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,6 +3,6 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git remote add origin https://${GITHUB_TOKEN}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
+git remote add origin https://${GITHUB_PUSH}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
 git status
 git push origin master 
