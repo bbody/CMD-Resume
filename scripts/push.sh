ba6d950a4f805a303ab0cbfaf91a682981ff85d4
Brendon Body <bbody.project@gmail.com>
2018-06-04 04:53:09 +1000

    :rocket: Add other endpoint

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,6 +3,5 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git remote add origin https://${GITHUB_PUSH}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
-git status
-git push origin master 
+git remote add upstream https://${GITHUB_PUSH}@github.com/bbody/CMD-Resume.git
+git push upstream master 
