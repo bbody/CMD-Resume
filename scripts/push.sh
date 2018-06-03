985888d15ccf59f4f081118796ab6cd76c0d7d50
Brendon Body <bbody.project@gmail.com>
2018-06-04 05:04:04 +1000

    :rocket: Remove dist from gitignore

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,5 +3,4 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git remote add upstream https://${GITHUB_PUSH}@github.com/bbody/CMD-Resume.git
 git push upstream master 
