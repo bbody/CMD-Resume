a9ba32a9b87156a3089f1350ae35d50ab5313eb0
Brendon Body <bbody.project@gmail.com>
2018-06-11 02:48:44 +1000

    :umbrella::microscope: Add more tests to increase code coverage

--- spec/splash.spec.js
+++ spec/splash.spec.js
@@ -67,26 +67,50 @@ describe("Splash screen", function() {
 		});
 	});
 
-	// describe("Custom splash", function() {
-	// 	beforeEach(function() {
-	// 		jasmine.Ajax.stubRequest('emptyStrings.json').andReturn({
-	// 			status: 200,
-	// 		    responseText: JSON.stringify(loadJSON("emptyStrings"))
-	// 		});
+	describe("Custom splash", function() {
+		describe("Single line", function() {
+			beforeEach(function() {
+				jasmine.Ajax.stubRequest('emptyStrings.json').andReturn({
+					status: 200,
+				    responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
 
-	// 		jasmine.Ajax.stubRequest('customSplash.json').andReturn({
-	// 			status: 200,
-	// 		    responseText: JSON.stringify(loadJSON("extraDetails/customSplash"))
-	// 		});
+				jasmine.Ajax.stubRequest('customSplash.json').andReturn({
+					status: 200,
+				    responseText: JSON.stringify(loadJSON("extraDetails/customSplash"))
+				});
 
-	// 		$("#cmd-resume").CMDResume("emptyStrings.json", {extraDetails: "customSplash.json"});
-	// 	});
+				$("#cmd-resume").CMDResume("emptyStrings.json", {extraDetails: "customSplash.json"});
+			});
 
-	// 	it("Includes the basic splash", function() {
-	// 		var splash = getSimpleSplash();
-	// 		expect(splash).toEqual("Custom splash");
-	// 	});
-	// });
+			it("Includes the basic splash", function() {
+				var splash = getSimpleSplash();
+				expect(splash).toEqual("Custom splash");
+			});
+		});
+
+		describe("Multiple lines", function() {
+			beforeEach(function() {
+				jasmine.Ajax.stubRequest('emptyStrings.json').andReturn({
+					status: 200,
+				    responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
+
+				jasmine.Ajax.stubRequest('customSplash.json').andReturn({
+					status: 200,
+				    responseText: JSON.stringify(loadJSON("extraDetails/largeCustomSplash"))
+				});
+
+				$("#cmd-resume").CMDResume("emptyStrings.json", {extraDetails: "customSplash.json"});
+			});
+
+			it("Includes the basic splash", function() {
+				var splash = getMultilineSplash();
+				expect(splash[0]).toEqual("Custom");
+				expect(splash[1]).toEqual("splash");
+			});
+		});
+	});
 
 	describe("Splash Command", ()=> {
 		describe("Featuring name", function() {
@@ -158,30 +182,30 @@ describe("Splash screen", function() {
 			});
 		});
 
-		// describe("Custom splash", function() {
-		// 	beforeEach(function() {
-		// 		jasmine.Ajax.stubRequest('emptyStrings.json').andReturn({
-		// 			status: 200,
-		// 		    responseText: JSON.stringify(loadJSON("emptyStrings"))
-		// 		});
+		describe("Custom splash", function() {
+			beforeEach(function() {
+				jasmine.Ajax.stubRequest('emptyStrings.json').andReturn({
+					status: 200,
+				    responseText: JSON.stringify(loadJSON("emptyStrings"))
+				});
 
-		// 		jasmine.Ajax.stubRequest('customSplash.json').andReturn({
-		// 			status: 200,
-		// 		    responseText: JSON.stringify(loadJSON("extraDetails/customSplash"))
-		// 		});
+				jasmine.Ajax.stubRequest('customSplash.json').andReturn({
+					status: 200,
+				    responseText: JSON.stringify(loadJSON("extraDetails/customSplash"))
+				});
 
-		// 		$("#cmd-resume").CMDResume("emptyStrings.json", {extraDetails: "customSplash.json"});
-		// 	});
+				$("#cmd-resume").CMDResume("emptyStrings.json", {extraDetails: "customSplash.json"});
+			});
 
-		// 	it("Includes the basic splash", function() {
+			it("Includes the basic splash", function() {
 
-		// 		enterCommand("splash");
+				enterCommand("splash");
 
-		// 		var lines = $(".terminal-output > div");
-		// 		var output = $(lines[lines.length - 1]).find("div span");
+				var lines = $(".terminal-output > div");
+				var output = $(lines[lines.length - 1]).find("div span");
 
-		// 		expect($(output[0]).html().decodeSpace()).toEqual("Custom splash");
-		// 	});
-		// });
+				expect($(output[0]).html().decodeSpace()).toEqual("Custom splash");
+			});
+		});
 	});
 });
