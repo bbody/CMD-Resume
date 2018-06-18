61639ff7f810289029b065371b8b535298d846e8
Brendon Body <bbody.project@gmail.com>
2018-06-18 20:44:04 +1000

    :rocket::snow_flake: Update binary

--- wdio.conf.js
+++ wdio.conf.js
@@ -65,7 +65,7 @@ exports.config = {
                 //     // to run chrome headless the following flags are required
                 //     // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
                 args: ['--headless', '--disable-gpu'],
-                binary: "chrome"
+                binary: "/usr/bin/google-chrome"
             }
         },
         {
