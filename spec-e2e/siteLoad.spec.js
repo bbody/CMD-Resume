1aa51c6f97341650e7aaeda3290d0627fe8723c5
Brendon Body <bbody.project@gmail.com>
2018-11-18 20:44:49 +1100

    :umbrella::microscope: Add tests which type commands

---
+++ spec-e2e/siteLoad.spec.js
@@ -0,0 +1,44 @@
+var helper = require('./support/helper.js');
+
+describe("Website title", function() {
+	beforeEach(function(){
+		browser.url('/');
+	});
+
+	it("changes once plugin loads", function(){
+		var title = browser.getTitle();
+		expect(title).toBe("Command Line Résumé");
+		browser.execute(function(){
+			$('body').CMDResume('./responses/details.json');
+		});
+
+		var terminal = $('body.terminal');
+
+		terminal.waitForExist(5000);
+
+		title = browser.getTitle();
+		expect(title).toBe("Richard Hendriks's Résumé");
+	});
+});
+
+describe("Website load", function() {
+	beforeEach(function(){
+		browser.execute(function(){
+			$('body').CMDResume('./responses/details.json');
+		});
+
+		$('body.terminal').waitForExist(5000);
+	});
+
+	it("has welcome message", function(){
+		var welcomeMessage = browser.getText('.terminal-output div[data-index="0"] div:first-child');
+
+		expect(welcomeMessage).toBe("Welcome to Richard Hendriks's résumé.");
+	});
+
+	it("has help message", function(){
+		var helpMessage = browser.getText('.terminal-output div[data-index="0"] div:last-child');
+
+		expect(helpMessage).toBe("Type help for commands");
+	});
+});
--- spec-e2e/support/helper.js
+++ spec-e2e/support/helper.js
@@ -1,6 +1,44 @@
 module.exports = {
 	loadCMDResume: function() {
-		console.log("HELLO");
 		$('body').CMDResume('./responses/details.json', {'extraDetails': './responses/extra-details.json'});
+	},
+	getSimpleValues: function(browser){
+		var key = browser.getText('.terminal-output div:last-child div:nth-child(1)');
+		var value = browser.getText('.terminal-output div:last-child div:nth-child(2)');
+
+		return {key: key, value: value};
+	},
+	keyboard: {
+		typeCharacters: function(browser, keys) {
+			var keys = [];
+			for (var i = 0; i < keys.length; i++){
+				var key = {
+					type: 'key',
+					id: `keyboard${i}`,
+					actions: [
+						{"type": "keyDown", "value": keys[i]},
+						{"type": "pause", "value": 500}
+					]
+				};
+				keys.push(key);
+			}
+
+			browser.actions(keys);
+
+		},
+		typeEnter: function(browser) {
+			browser.actions([
+				{
+					type: 'key',
+					id: 'keyboard',
+					actions: [
+						{"type": "keyDown", "value": "a"},
+						{"type": "pause", "value": 500}
+					]
+				}
+			]);
+
+			browser.actions();
+		}
 	}
-}
+};
