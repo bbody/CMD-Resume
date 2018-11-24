d5dafebb6d6306a4b7490dffc1bfbd4b2feff1f1
Brendon Body <bbody.project@gmail.com>
2018-11-24 17:48:06 +1100

    :newspaper: Script to generate browsers list

---
+++ scripts/generate-browserstack-karma-browsers.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+
+var getListOfVersions = function(start, end) {
+	var versions = [];
+
+	for (var i = start; i <= end; i++) {
+		versions.push(`${i}`);
+	}
+
+	return versions;
+};
+
+var config = require('../bs-browsers.config.json');
+var browserMap = {};
+var browserList = [];
+for (var os of config.operating_systems) {
+	for (var browser of os.browsers) {
+		if (browser.enabled) {
+			if (!browser.versions){
+				browser.versions = getListOfVersions(browser.oldest, browser.newest);
+			}
+
+			for (var version of browser.versions) {
+				var key = `bs__${os.name.replace(' ', '-')}_${os.version.replace(' ', '-')}__${browser.name}_${version}`;
+				browserList.push(key);
+				browserMap[key] = {
+					"base": "BrowserStack",
+					"browser": browser.name,
+					"browser_version": version,
+					"os": os.name,
+					"os_version": os.version
+				}
+			}
+		}
+	}
+}
+
+var content = JSON.stringify(browserMap);
+fs.writeFile('bs-customLaunchers.json', content, function(err, data){
+	if (err) {
+		console.error(err);
+	}
+	fs.writeFile('bs-browerList.json', JSON.stringify({browsers: browserList}), function(err, data){
+		if (err) {
+			console.error(err);
+		}
+	});
+});
