d0e3b59d23f709a8f0f0d62d051c1a0800d49ba2
Brendon Body <bbody.project@gmail.com>
2018-12-01 17:27:38 +1100

    :umbrella: Add tests for new function

--- spec/helpers/misc.spec.js
+++ spec/helpers/misc.spec.js
@@ -81,4 +81,56 @@ describe("misc", function() {
 			expect(isValidColor()).toBeFalsy();
 		});
 	});
+
+	describe("Color validator", function() {
+		it("Hex code (3 digits)", function() {
+			expect(isValidColor("#FFF")).toBeTruthy();
+		});
+
+		it("Hex code (6 digits)", function() {
+			expect(isValidColor("#C0FFEE")).toBeTruthy();
+		});
+
+		it("Color name", function() {
+			expect(isValidColor("white")).toBeTruthy();
+		});
+
+		it("Invalid color name", function() {
+			expect(isValidColor("test")).toBeFalsy();
+		});
+
+		it("Empty string", function() {
+			expect(isValidColor("")).toBeFalsy();
+		});
+
+		it("Empty string", function() {
+			expect(isValidColor(null)).toBeFalsy();
+		});
+
+		it("Empty string", function() {
+			expect(isValidColor()).toBeFalsy();
+		});
+	});
+
+	describe("Object has key defined and not empty", function() {
+		it("Handles no key", function() {
+			expect(isDefinedNotEmpty({}, "key")).toBeFalsy();
+		});
+
+		it("Has string with length", function() {
+			expect(isDefinedNotEmpty({"key": ""}, "key")).toBeFalsy();
+		});
+
+		it("Has string with length", function() {
+			expect(isDefinedNotEmpty({"key": "a"}, "key")).toBeTruthy();
+		});
+
+		it("Handles empty array", function() {
+			expect(isDefinedNotEmpty({"key": []}, "key")).toBeFalsy();
+		});
+
+		it("Handles array", function() {
+			expect(isDefinedNotEmpty({"key": ["value"]}, "key")).toBeTruthy();
+		});
+	});
 });
