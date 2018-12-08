79b5c9cf601381b257dea5677adf64a00601b610
Brendon Body <bbody.project@gmail.com>
2018-12-08 14:13:11 +1100

    :rocket: Run BS

--- wdio.bs-essential.conf.js
+++ wdio.bs-essential.conf.js
@@ -1,15 +1,16 @@
 var browserstack = require('browserstack-local');
-let wdioConf = require("./wdio.conf.js");
+let wdioConf = require("./wdio.conf.js").config;
 var merge = require('deepmerge');
 
 var capabilities = require('./browserstack/bs-customLaunchers.essential.json').browsers;
 
+var jobId = process && process.env && process.env.TRAVIS_JOB_ID ? process.env.TRAVIS_JOB_ID : "local development";
+var buildName = `UI Build: ${jobId}`
 for (var i = 0; i < capabilities.length; i++) {
 	capabilities["browserstack.local"] = true;
 	capabilities.build = `UI Build: ${process.env.TRAVIS_JOB_ID}`;
+	capabilities.project = `UI Build: ${process.env.TRAVIS_JOB_ID}`;
 }
-
-// console.log(process.env.BROWSERSTACK_USERNAME);
 
 exports.config = merge(wdioConf, {
 	user: process.env.BROWSER_STACK_USERNAME,
