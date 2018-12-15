d9401679ce2302748dd2374b0e3bda020a5ce754
Brendon Body <bbody.project@gmail.com>
2018-12-15 19:46:15 +1100

    :rocket: Move back to script

--- scripts/doc-release.js
+++ scripts/doc-release.js
@@ -1,6 +1,6 @@
 let exec = require('child_process').exec;
 
-exec(`git add ./CMD-RESUME-DATA-SCHEMA.md ./README.md ./CONTRIBUTING.md && git commit -m ":books: Compile documentation" && git push origin doc-build`, function(error, stout, sterr) {
+exec(`git add CMD-RESUME-DATA-SCHEMA.md README.md CONTRIBUTING.md && git commit -m ":books: Compile documentation" && git push origin doc-build`, function(error, stout, sterr) {
 	if (stout) {
 		console.log(stout);
 	}
