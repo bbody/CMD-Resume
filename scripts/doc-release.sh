de955a07689354670df99b60c05143d9a47e62a0
Brendon Body <bbody.project@gmail.com>
2018-12-15 20:04:10 +1100

    :snowflake: Use script

---
+++ scripts/doc-release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout doc-build
+  git add CMD-RESUME-DATA-SCHEMA.md README.md CONTRIBUTING.md
+  git commit --message ":books: Compile documentation"
+}
+
+upload_files() {
+  git remote add origin https://${GITHUB_PUSH}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin doc-build 
+}
+
+setup_git
+commit_website_files
+upload_files
