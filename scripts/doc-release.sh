c35229ad37dd546ad820ccf13aa35637025e6c2f
Travis CI <travis@travis-ci.org>
2018-12-17 23:19:03 +1100

    :rocket: Only run on master

--- scripts/doc-release.sh
+++ scripts/doc-release.sh
@@ -5,7 +5,7 @@ GH_TOKEN=$2
 git config --global user.email "support@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git checkout doc-build
+git checkout master
 git reset
 git add CMD-RESUME-DATA-SCHEMA.md README.md CONTRIBUTING.md
 git commit --message ":books: Compile documentation for version $PACKAGE_VERSION"
@@ -14,4 +14,4 @@ git commit --message ":gem::bookmark: Compile for version $PACKAGE_VERSION relea
 
 git remote remove origin
 git remote add origin https://bbody:${GH_TOKEN}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
-git push --quiet --set-upstream origin doc-build 
+git push --quiet --set-upstream origin master 
