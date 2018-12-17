983ef9480ff8f93e7a14046e4ffe84b3f3e9e4d8
Travis CI <travis@travis-ci.org>
2018-12-17 20:33:53 +1100

    :snowflake::fire: Remove JS script and fix shell script to use environment variables

--- scripts/doc-release.sh
+++ scripts/doc-release.sh
@@ -1,26 +1,16 @@
 #!/bin/sh
+PACKAGE_VERSION=$(eval echo $PACKAGE_VERSION)
+GH_TOKEN=$(eval echo $GH_TOKEN)
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-commit_website_files() {
-  git checkout doc-build
-  git reset
-  git add CMD-RESUME-DATA-SCHEMA.md
-  git add README.md
-  git add CONTRIBUTING.md
-  git commit --message ":books: Compile documentation for version version $PACKAGE_VERSION"
-  git add dist/*
-  git commit --message ":gem::bookmark: Compile for version $PACKAGE_VERSION release"
-}
+git checkout doc-build
+git reset
+git add CMD-RESUME-DATA-SCHEMA.md README.md CONTRIBUTING.md
+git commit --message ":books: Compile documentation for version $PACKAGE_VERSION"
+git add dist/*
+git commit --message ":gem::bookmark: Compile for version $PACKAGE_VERSION release"
 
-upload_files() {
-  git remote add origin https://bbody:${GH_TOKEN}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin doc-build 
-}
-
-setup_git
-commit_website_files
-upload_files
+git remote add origin https://bbody:${GH_TOKEN}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
+git push --quiet --set-upstream origin doc-build 
