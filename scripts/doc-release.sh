b756ce4d0da2125e00ce6241c9ba639a95eddbff
Travis CI <travis@travis-ci.org>
2018-12-17 22:34:29 +1100

    :snowflake: Re-add username

--- scripts/doc-release.sh
+++ scripts/doc-release.sh
@@ -12,5 +12,6 @@ git commit --message ":books: Compile documentation for version $PACKAGE_VERSION
 git add dist/*
 git commit --message ":gem::bookmark: Compile for version $PACKAGE_VERSION release"
 
-git remote add origin https://${GH_TOKEN}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
+git remote remove origin
+git remote add origin https://bbody:${GH_TOKEN}@github.com/bbody/CMD-Resume.git > /dev/null 2>&1
 git push --quiet --set-upstream origin doc-build 
