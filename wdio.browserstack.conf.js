5b04a4e2f1f680db5944d7278154cd0e8c4fa9f4
Brendon Body <bbody.project@gmail.com>
2018-12-24 04:46:44 +1100

    :checkered_flag::snowflake: Only run basic tests for IE (#277)

--- wdio.browserstack.conf.js
+++ wdio.browserstack.conf.js
@@ -5,18 +5,24 @@ var merge = require('deepmerge');
 var capabilities = require('./browserstack/bs-customLaunchers.essential.json').browsers;
 
 // Set a build name based off build number
-var jobId = process && process.env && process.env.TRAVIS_BUILD_NUMBER ? process.env.TRAVIS_BUILD_NUMBER : "local development";
+var jobId = process && process.env && process.env.TRAVIS_BUILD_NUMBER ? process.env.TRAVIS_BUILD_NUMBER : "local_development";
 var buildName = `UI ${jobId}`;
 for (var i = 0; i < capabilities.length; i++) {
 	capabilities[i].build = buildName;
+	console.log(capabilities[i].specs);
+	if (!capabilities[i].specs) {
+		capabilities[i].specs = ["spec-e2e/*.spec.js"];
+	}
 }
 
 wdioConf.services.push('browserstack');
+wdioConf.specs = [];
 
 exports.config = merge(wdioConf, {
 	user: process.env.BROWSER_STACK_USERNAME,
 	key: process.env.BROWSER_STACK_ACCESS_KEY,
 	browserstackLocal: true,
 	capabilities: capabilities,
-	maxInstances: 5
+	maxInstances: 5,
+	bail: 1
 });
