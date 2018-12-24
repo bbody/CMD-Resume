2c371260ea19eec302b3d1f71d9ae623a034f4f3
Brendon Body <bbody.project@gmail.com>
2018-12-24 17:26:37 +1100

    :snowflake: Attempt full run

--- wdio.browserstack.conf.js
+++ wdio.browserstack.conf.js
@@ -2,14 +2,13 @@ var browserstack = require('browserstack-local');
 let wdioConf = require("./wdio.conf.js").config;
 var merge = require('deepmerge');
 
-var capabilities = require('./browserstack/bs-customLaunchers.essential.json').browsers;
+var capabilities = require('./browserstack/bs-customLaunchers.all.json').browsers;
 
 // Set a build name based off build number
 var jobId = process && process.env && process.env.TRAVIS_BUILD_NUMBER ? process.env.TRAVIS_BUILD_NUMBER : "local_development";
 var buildName = `UI ${jobId}`;
 for (var i = 0; i < capabilities.length; i++) {
 	capabilities[i].build = buildName;
-	console.log(capabilities[i].specs);
 	if (!capabilities[i].specs) {
 		capabilities[i].specs = ["spec-e2e/*.spec.js"];
 	}
