38b27a1e87fb3df7aacd41f51636ebce11839ed8
Brendon Body <bbody.project@gmail.com>
2018-12-24 20:05:58 +1100

    :ambulance: Remove bug in WDIO

--- wdio.browserstack.base.conf.js
+++ wdio.browserstack.base.conf.js
@@ -9,7 +9,6 @@ exports.config = merge(wdioConf, {
 	user: process.env.BROWSER_STACK_USERNAME,
 	key: process.env.BROWSER_STACK_ACCESS_KEY,
 	browserstackLocal: true,
-	capabilities: capabilities,
 	maxInstances: 5,
 	bail: 1,
 	getBrowserList: function(fileName) {
--- wdio.browserstack.essential.conf.js
+++ wdio.browserstack.essential.conf.js
@@ -1,7 +1,7 @@
 let wdioConf = require("./wdio.browserstack.base.conf.js").config;
 var merge = require('deepmerge');
 
-
+console.log(wdioConf.getBrowserList('./browserstack/bs-customLaunchers.essential.json'));
 exports.config = merge(wdioConf, {
-	capabilities: wdioConf.('./browserstack/bs-customLaunchers.essential.json')
+	capabilities: wdioConf.getBrowserList('./browserstack/bs-customLaunchers.essential.json')
 });
