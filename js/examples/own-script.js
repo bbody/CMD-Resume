28ce53e767506c73615745c6acb77c8a00dc6ba4
Brendon Body <bbody.project@gmail.com>
2018-12-29 18:49:24 +1100

    :lint: Fix linting on misc JS files

--- js/examples/own-script.js
+++ js/examples/own-script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    var settings = {
-        showForks: true,
-        extraDetails: "../responses/my-extra-details.json"
-    };
-    
-    $("body").CMDResume("https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json", settings);
+	var settings = {
+		showForks: true,
+		extraDetails: '../responses/my-extra-details.json'
+	};
+
+	$('body').CMDResume('https://s3-ap-southeast-2.amazonaws.com/bbody/Details/details.json', settings);
 });
