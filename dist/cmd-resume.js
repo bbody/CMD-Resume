296405e638546054f6dcd9b42cb9ffcbafee4b45
Travis CI <support@travis-ci.org>
2019-01-01 08:35:59 +0000

    :gem::bookmark: Compile for version 5.4.1 release

--- dist/cmd-resume.js
+++ dist/cmd-resume.js
@@ -1,4 +1,4 @@
-/* v5.4.1 of CMD Resume by Brendon Body(https://github.com/bbody/CMD-Resume.git) */
+/* v5.4.1 of CMD Resume by Brendon Body (https://github.com/bbody/CMD-Resume.git) */
 ;(function($){
   "use strict";
   
